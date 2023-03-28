d50940a409d1a2233efb4bccd235ae81a41b56a6
Abanoub-Samy <21p0356@eng.asu.edu.eg>
2023-03-29 01:20:30 +0200

    Displaying video when uploading on course edit page (using javascript)

---
+++ videoDisplay.js
@@ -0,0 +1,13 @@
+function selectedVideo(self){
+    var file = self.files[0];
+    var reader = new FileReader();
+    reader.onload = function(e){
+        var src = e.target.result;
+        var video = document.getElementById("video");
+        var source = document.getElementById("source");
+        source.setAttribute("src",src);
+        video.load();
+        video.play();
+    }
+    reader.readAsDataURL(file);
+}
