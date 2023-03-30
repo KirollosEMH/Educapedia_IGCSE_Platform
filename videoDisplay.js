d277ad117d7e2fdf9c685458aa5271732f750f33
AHKSASE2002 <21P0153@eng.asu.edu.eg>
2023-03-30 05:43:59 +0200

    Uploading Video must be a video

--- videoDisplay.js
+++ videoDisplay.js
@@ -7,7 +7,23 @@ function selectedVideo(self){
         var source = document.getElementById("source");
         source.setAttribute("src",src);
         video.load();
-        video.play();
+        //video.play();
     }
+    document.getElementById("video").value = "It is not undefined"
     reader.readAsDataURL(file);
 }
+
+const fileInput = document.querySelector('input[type="file"]');
+
+fileInput.addEventListener('change', function(event) {
+
+  const file = event.target.files[0];
+
+  const allowedExtensions = /(\.mp4|\.mov|\.avi|\.wmv)$/i;
+  if (!allowedExtensions.exec(file.name)) {
+    alert('Please select a video file with .mp4, .mov, .avi, or .wmv extension.');
+    fileInput.value = '';
+    return;
+  }
+  submitForms();
+});
