035c9879d22f6d119142df58a6680e3912b0140c
Ahmed-Nezar <125882735+Ahmed-Nezar@users.noreply.github.com>
2023-03-30 05:47:29 +0200

    Made the user Upload all the fields required on course edit page

--- SubmitTwoForms.js
+++ SubmitTwoForms.js
@@ -2,14 +2,20 @@ submitForms = function(){
     var video = document.getElementById("video").value;
     var pdfFile = document.getElementById("pdfFile").value;
     var quizUrl = document.getElementById("Quiz URl").value;
-  
-    if (video == "" || pdfFile == "" || quizUrl == "") {
+    if(video == undefined ){
       alert("Please fill out all the required fields.");
-    } else {
+    }
+    else if (pdfFile == "" ) {
+      alert("Please fill out all the required fields")
+    }
+    else if (quizUrl == "") {
+      alert("Please fill out all the required fields")
+    }
+    else if (video !== undefined && pdfFile !== "" && quizUrl !== "") { //upload all at a time
+      alert("Upload Done");
       document.getElementById("form1").submit();
       document.getElementById("form2").submit();
-      alert("Upload Done");
       window.location.href = "CourseControl.html";
-    }
+}
   }
   
